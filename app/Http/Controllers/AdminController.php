94c6f2ff72b4fc6d901a9fa6efd873577f0e18d8
abdullahamdy <abdullahhamdy29@gmail.com>
2023-09-03 16:39:13 +0200

    add DB transaction in CreateBlog Function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,6 +11,7 @@ use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -273,26 +274,32 @@ class AdminController extends Controller
 
     public function createBlog(Request $request)
     {
-        // dd($request->all());
-        $blog = Blog::create([
-            'title' => $request->title,
-            'post' => $request->post,
-            'post_excerpt' => $request->post_excerpt,
-            'metaDescription' => $request->metaDescription,
-            'user_id' => Auth::id(),
-            'jsonData' => $request->jsonData,
-            'slug' => '',
-        ]);
-        $blogCategories = [];
-        foreach ($request->category_id as $ca) {
-            array_push($blogCategories, ['category_id' => $ca, 'blog_id' => $blog->id]);
+        DB::beginTransaction();
+        try {
+            $blog = Blog::create([
+                'title' => $request->title,
+                'post' => $request->post,
+                'post_excerpt' => $request->post_excerpt,
+                'metaDescription' => $request->metaDescription,
+                'user_id' => Auth::id(),
+                'jsonData' => $request->jsonData,
+                'slug' => '',
+            ]);
+            $blogCategories = [];
+            foreach ($request->category_id as $ca) {
+                array_push($blogCategories, ['category_id' => $ca, 'blog_id' => $blog->id]);
+            }
+            Blogcategory::insert($blogCategories);
+            $blogTags = [];
+            foreach ($request->tag_id as $ta) {
+                array_push($blogTags, ['tag_id' => $ta, 'blog_id' => $blog->id]);
+            }
+            Blogtag::insert($blogTags);
+            DB::commit();
+            return 'done';
+        } catch (\Throwable) {
+            DB::rollBack();
+            return 'not done';
         }
-        Blogcategory::insert($blogCategories);
-        $blogTags = [];
-        foreach ($request->tag_id as $ta) {
-            array_push($blogTags, ['tag_id' => $ta, 'blog_id' => $blog->id]);
-        }
-        Blogtag::insert($blogTags);
-        return 'done';
     }
 }
